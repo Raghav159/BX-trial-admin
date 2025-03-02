aab93b08c1cc3075f32d3b95d807dc25279ee8bf
DilipSC <dilipchakravarthi570@gmail.com>
2025-03-02 23:18:54 +0530

    jwt included in login

--- SERVER/api/auth/login.py
+++ SERVER/api/auth/login.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from prisma.models import User as PrismaUser
+import jwt
 
 class User(BaseModel):
     email:str
@@ -16,6 +17,8 @@ async def login(user:User):
         return {"error":"User not found"}
     else:
         if prisma_user.password==user.password :
-            return {"fullName":prisma_user.fullName,"email":prisma_user.email,"role":prisma_user.role}
+            payload={"id":prisma_user.id,"email":prisma_user.email,"role":prisma_user.role}
+            token=jwt.encode(payload,'jaidboss',algorithm='HS256',)
+            return {"fullName":prisma_user.fullName,"email":prisma_user.email,"role":prisma_user.role,"token":token}
         else:
             return {"error":"Incorrect password"}
